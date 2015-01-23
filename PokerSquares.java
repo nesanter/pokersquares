fc475dacd99b40cc1aa0969a9e9cb5539846706d
nesanter <nesanter@knox.edu>
2015-01-22 19:05:47 -0600

    Readd scanner import

--- PokerSquares.java
+++ PokerSquares.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 import java.util.Stack;
+import java.util.Scanner;
 
 /* The solitaire game "Poker Squares"
  * Author: Todd W. Neller
