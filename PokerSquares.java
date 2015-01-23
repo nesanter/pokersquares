9c4a5ddccc0a7499c3410f994dba1d30ce8c0803
runfrodorun <frodorocks1972@gmail.com>
2015-01-22 18:49:33 -0600

    Added interface to pick the player and start the program

--- PokerSquares.java
+++ PokerSquares.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 import java.util.Stack;
+import java.util.Scanner;
 
 /* The solitaire game "Poker Squares"
  * Author: Todd W. Neller
@@ -344,7 +345,27 @@ public class PokerSquares {
      * @param args
      */
     public static void main(String[] args) {
-        //new PokerSquares(new RandomPokerSquaresPlayer(), GAME_MILLIS).playSequence(10000, 0, false);
-        //new PokerSquares(new SimplePokerSquaresPlayer(), GAME_MILLIS).playSequence(100, 0, false);
+
+        Scanner s = new Scanner( System.in );
+        System.out.println( "Welcome to poker squares!" );
+        System.out.println( "  Pick a player to use:" );
+        System.out.println( "    0 - Exit" );
+        System.out.println( "    1 - Random Player" );
+        System.out.println( "    2 - Flush Player" );
+
+        switch( s.nextInt() )
+        {
+            case 0:
+                break;
+            case 1:
+                new PokerSquares(new RandomPokerSquaresPlayer(), GAME_MILLIS).playSequence(5, 0, false);
+                break;
+            case 2:
+                new PokerSquares(new FlushPokerSquaresPlayer(), GAME_MILLIS).playSequence(5, 0, false);
+                break;
+            default:
+                System.out.println( "That's not a vaild player choice!" );
+                break;
+        }
     }
 }
